7f3d29e3d70b7b389ac4ad59b868c2991b3c6d12
arsi <natayudaarsi@gmail.com>
2021-10-18 01:11:08 +0700

    Deploy portfolio

---
+++ precache-manifest.9e629e85f4d602d9a689d2a7a6608c29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab9e370b71235d0bbe334f54cfa369a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdab9bbe9224b47e7cbc",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "869c9dcec8ad5ff7db49",
+    "url": "/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "bdab9bbe9224b47e7cbc",
+    "url": "/static/js/2.6d9105ba.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.6d9105ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869c9dcec8ad5ff7db49",
+    "url": "/static/js/main.997880f7.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
